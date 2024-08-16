ab0f64c241f293617ef298997e5217cd92dfdef2
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2024-08-17 04:49:55 +0530

    chore: bump deps
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- packages/canvas-module/playground/app/pages/index.vue
+++ packages/canvas-module/playground/app/pages/index.vue
@@ -34,16 +34,16 @@
 
 <template>
   <div v-if="isReady">
-    <div class="p-4 grid grid-cols-1 gap-3">
+    <div class="grid grid-cols-1 gap-3 p-4">
       <template v-if="user">
         <user-info :user="user" @open="openUserProfile" />
       </template>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <div class="grid grid-cols-1 gap-6 md:grid-cols-3">
         <button
           v-for="height in [1000, 1500, 0]"
           :key="height"
           type="button"
-          class="text-white font-bold py-2 px-4 rounded bg-gray-500 hover:bg-gray-400 hover:border-gray-500"
+          class="px-4 py-2 font-bold text-white bg-gray-500 rounded hover:bg-gray-400 hover:border-gray-500"
           @click="setBodyHeight(height)"
         >
           {{ height ? `Set height to ${height}px` : 'Reset height' }}
@@ -53,10 +53,10 @@
   </div>
   <div
     v-else
-    class="w-dvw h-dvh flex flex-col gap-y-4 items-center justify-center"
+    class="flex flex-col items-center justify-center w-dvw h-dvh gap-y-4"
   >
     <svg
-      class="animate-spin size-12 text-white"
+      class="text-white animate-spin size-12"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
@@ -68,12 +68,12 @@
         r="10"
         stroke="currentColor"
         stroke-width="4"
-      ></circle>
+      />
       <path
         class="opacity-75"
         fill="currentColor"
         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-      ></path>
+      />
     </svg>
     Loading canvas...
   </div>
